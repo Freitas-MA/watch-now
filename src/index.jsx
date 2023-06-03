b0761a2fe07af2411a412a7ca21fe87ce3d2e6c5
Marcos Freitas <freitas.ma.1993@gmail.com>
2023-06-03 15:19:24 +0100

    updade body scroll

--- src/index.jsx
+++ src/index.jsx
@@ -5,8 +5,15 @@ import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-
 document.body.style.marginTop = '50px';
 
 root.render(<App />);
+
+// Add the following CSS rule to the root element
+document.getElementById('root').style.width = '100vw';
+document.getElementById('root').style.overflowX = 'hidden';
+
+// Add the following CSS rule to the App component
+document.body.style.overflow = 'hidden';
+
 
